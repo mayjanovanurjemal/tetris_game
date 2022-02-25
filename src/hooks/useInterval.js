97164e6c7613379358cf7728671bb22c069112b4
kyyasilmyradovv <kyyasilmyradov2021@gmail.com>
2022-02-25 16:03:54 +0500

    first

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+export function useInterval(callback, delay) {
+	const savedCallBack = useRef();
+	useEffect(() => {
+		savedCallBack.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		function tick() {
+			savedCallBack.current();
+		}
+		if (delay !== null) {
+			const id = setInterval(tick, delay);
+			return () => {
+				clearInterval(id);
+			};
+		}
+	}, [delay]);
+};
